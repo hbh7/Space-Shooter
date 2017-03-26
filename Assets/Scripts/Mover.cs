f4e8efb9205fc7e08137123ddba6dbc6cab03d3b
hbh7 <hbh7@hbh7.com>
2017-03-26 15:16:10 -0400

    1.6 Complete

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    private new Rigidbody rigidbody { get { return GetComponent<Rigidbody>(); } }
+    public float speed;
+
+    void Start()
+    {
+        
+        rigidbody.velocity = transform.forward * speed; 
+
+    } 
+}
