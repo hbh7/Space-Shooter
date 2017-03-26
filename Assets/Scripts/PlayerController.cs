1e17e6398c3449ed36f87af1c20bf1d12768cfc3
hbh7 <hbh7@hbh7.com>
2017-03-26 00:11:37 -0400

    1.5 Complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    private new Rigidbody rigidbody { get { return GetComponent<Rigidbody>(); } }
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidbody.velocity = movement * speed;
+
+        rigidbody.position = new Vector3
+            (
+                Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+            );
+
+        rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+    }
+}
