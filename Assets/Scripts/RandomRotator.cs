10650c320c56608fac3a25f2cf39949d0c12a741
hbh7 <hbh7@hbh7.com>
2017-03-26 20:56:58 -0400

    2.2 Complete

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+    private new Rigidbody rigidbody { get { return GetComponent<Rigidbody>(); } }
+    public float tumble;
+
+    private void Start()
+    {
+        rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
